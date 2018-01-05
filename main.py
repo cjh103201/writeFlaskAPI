642510bb87ac3252a7246da9cd7cc4fac188ca8d
rnder006 <rnder_006@iirtech.co.kr>
2018-01-06 02:35:09 +0900

    server test

--- main.py
+++ main.py
@@ -28,9 +28,5 @@ class Write(Resource):
 api.add_resource(RegistUser, '/user')
 api.add_resource(Write, '/write')
 
-@app.route('/')
-def hell_world():
-    return "Hello! World!"
-
 if __name__ == '__main__':
     app.run(debug=True)
